02ac57c1a6041939b11125b8250dd91dae70f4f6
Marlon Strong <tsenane14@gmail.com>
2021-01-25 01:53:07 +0200

    created repositories component on dashboard

--- src/Pages/Dashboard/DashElements.js
+++ src/Pages/Dashboard/DashElements.js
@@ -43,3 +43,39 @@ button{
     }
 }
 `
+
+export const Repositories=styled.div`
+margin-top:80px;
+max-width: 700px;
+
+a{
+    background:#fff;
+    border-radius:5px;
+    width:100%;
+    padding:24px;
+    display:block;
+    text-decoration: none;
+    display: flex;
+    align-items:center;
+}
+
+img{
+    width:64px;
+    height:64px;
+    border-radius:50%;
+}
+
+div{
+    margin-left:16px;
+
+}
+strong{
+    font-size:20px;
+    color: #3D3D4D;
+}
+
+p{
+    font-size: 18px;
+    color:#A8A8B3;
+}
+`
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Title, Form } from './DashElements'
+import { Title, Form, Repositories } from './DashElements'
 import logo from '../../assets/logo.svg'
 
 function Dashboard() {
@@ -11,6 +11,16 @@ function Dashboard() {
                 <input placeholder="Write the name of the repo"/>
                 <button type="submit">Search</button>
             </Form>
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/65548563?s=460&u=aeb76087c3f98948fe4228b17831d4d7cefb17ed&v=4" alt="Marlon D Jesus"></img>
+                    <div>
+                        <strong>send</strong>
+                        <p>This a SPA Landing page of `Send`</p>
+                    </div>
+                
+                </a>
+            </Repositories>
         </>
     )
 }
