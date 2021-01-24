f36e419334e1d92cf1b943e70a404a5e3bc51067
Marlon Strong <tsenane14@gmail.com>
2021-01-24 16:49:23 +0200

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Routes from './Routes/';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
     <>
-<h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes/>    
+    </BrowserRouter>
+
 
     </>
   );
