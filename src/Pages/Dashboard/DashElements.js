3d7bf950417dd1dec8e3cac1e451b6963571027d
Marlon Strong <tsenane14@gmail.com>
2021-01-24 23:39:43 +0200

    added form, input & button components

--- src/Pages/Dashboard/DashElements.js
+++ src/Pages/Dashboard/DashElements.js
@@ -4,5 +4,10 @@ import styled from 'styled-components';
 export const Title= styled.h1`
 font-size: 48px;
 color: #3A3A3A;
+margin-top:80px;
+max-width: 450px;
+line-height: 56px;
+`
+export const Form=styled.form`
 
 `
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Title } from './DashElements'
+import { Title, Form } from './DashElements'
 import logo from '../../assets/logo.svg'
 
 function Dashboard() {
     return (
         <>
-            <img src={logo}/>
+            <img src={logo} alt="Github Explorer"/>
             <Title>Explore repos on GitHub</Title>
+            <Form action="">
+                <input placeholder="Write the name of the repo"/>
+                <button type="submit">Search</button>
+            </Form>
         </>
     )
 }
