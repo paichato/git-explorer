8bf2dd91e1ceadb6d19f0b9427dd10702feec5c1
Marlon Strong <tsenane14@gmail.com>
2021-01-25 00:20:07 +0200

    added styles for form, input & button components

--- src/Pages/Dashboard/DashElements.js
+++ src/Pages/Dashboard/DashElements.js
@@ -9,5 +9,26 @@ max-width: 450px;
 line-height: 56px;
 `
 export const Form=styled.form`
+margin-top: 40px;
+max-width: 700px;
+display:flex;
 
+input{
+    flex: 1;
+    height:70px;
+    padding: 0 24px;
+    border:0;
+    border-radius: 5px 0 0 5px;
+}
+
+button{
+    width: 210px;
+    max-width: 210px;
+    background:#04D361;
+    border-radius: 0 5px 5px 0;
+    outline: none;
+    border: none;
+    color: white;
+    font-weight: bold;
+}
 `
