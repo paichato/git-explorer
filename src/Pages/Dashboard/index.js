6c7f0144802bb0ec608e7ee348ce6f36b92ce157
Marlon Strong <tsenane14@gmail.com>
2021-01-25 16:00:09 +0200

    created folder services & api.js & added axios -D with baseUrl

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -21,22 +21,8 @@ function Dashboard() {
                     </div>
                 <FiChevronRight size={20}/>
                 </a>
-                <a href="teste">
-                    <img src="https://avatars.githubusercontent.com/u/65548563?s=460&u=aeb76087c3f98948fe4228b17831d4d7cefb17ed&v=4" alt="Marlon D Jesus"></img>
-                    <div>
-                        <strong>send</strong>
-                        <p>This a SPA Landing page of `Send`</p>
-                    </div>
-                <FiChevronRight size={20}/>
-                </a>
-                <a href="teste">
-                    <img src="https://avatars.githubusercontent.com/u/65548563?s=460&u=aeb76087c3f98948fe4228b17831d4d7cefb17ed&v=4" alt="Marlon D Jesus"></img>
-                    <div>
-                        <strong>send</strong>
-                        <p>This a SPA Landing page of `Send`</p>
-                    </div>
-                <FiChevronRight size={20}/>
-                </a>
+                
+                
             </Repositories>
         </>
     )
