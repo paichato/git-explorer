7510d20bf03df6d4a47b846054671b8fcd70f763
Marlon Strong <tsenane14@gmail.com>
2021-01-26 17:18:18 +0200

    Header styled

--- src/Pages/Repository/RepoElements.js
+++ src/Pages/Repository/RepoElements.js
@@ -5,11 +5,20 @@ display: flex;
 align-items: center;
 justify-content:space-between;
 
-a{
-    display: flex;
-    align-items:center;
-    text-decoration:none;
-    color: #a8a8b3;
-}
+    a{
+        display: flex;
+        align-items:center;
+        text-decoration:none;
+        color: #a8a8b3;
+        transition: all 0.2s ease-in-out;
+
+        &:hover{
+            color: #666;
+        }
+    }
+    svg{
+        margin-right:4px;
+    }
+
 
 `
